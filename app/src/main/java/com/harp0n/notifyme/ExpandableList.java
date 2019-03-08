7c446c4956b2b42a7fa9fec61fe4b12291effe19
Sebastian Pięta <44878991+KraftUnderscore@users.noreply.github.com>
2019-03-08 22:09:06 +0100

    Dodanie expandablelist
    Dodanie funkcjonującej już expandable list, możliwość wyświetlania w niej informacji o powiadomieniach, przyciski do przenoszenia do kolejnych activity.

---
+++ app/src/main/java/com/harp0n/notifyme/ExpandableList.java
@@ -0,0 +1,162 @@
+package com.harp0n.notifyme;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ExpandableList extends BaseExpandableListAdapter {
+    private final int TITLE_TXT_SIZE = 40;
+    private final int EXP_TXT_SIZE = 16;
+
+    private Context context;
+    private Notify[] notifies;
+
+    public ExpandableList(Context context, Notify[] notifies){
+        this.context = context;
+        this.notifies = notifies;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return notifies.length;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return 1;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return notifies[groupPosition].getName();
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        //return notifies[groupPosition].toString();
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        TextView tv = new TextView(context);
+        tv.setText((String)getGroup(groupPosition));
+        tv.setTextSize(TITLE_TXT_SIZE);
+        tv.setPadding(100,10,20,10);
+        return tv;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+
+        Notify n = notifies[groupPosition];
+
+        LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout buttons = new LinearLayout(context);
+        buttons.setOrientation(LinearLayout.HORIZONTAL);
+
+        Button deleteButton = new Button(context);
+        Button editButton = new Button(context);
+
+        TextView nameView = new TextView(context);
+        TextView descriptionView = new TextView(context);
+        TextView messageView = new TextView(context);
+        TextView isOneTimeView = new TextView(context);
+        TextView volumeChangeView = new TextView(context);
+        TextView wifiChangeView = new TextView(context);
+        TextView bluetoothChangeView = new TextView(context);
+        TextView alarmSoundOnView = new TextView(context);
+        TextView[] views = {nameView, descriptionView, messageView, isOneTimeView, volumeChangeView, wifiChangeView,
+                bluetoothChangeView, alarmSoundOnView};
+
+        nameView.setText(n.getName());
+        descriptionView.setText(n.getDiscription());
+        messageView.setText(n.getNotificationMessage());
+        deleteButton.setText("USUN");
+        editButton.setText("EDIT");
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO serialization.remove(n.getID())
+            }
+        });
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(context, NotifyEditor_Activity.class);
+                context.startActivity(myIntent);
+            }
+        });
+
+        //TODO replace hardcoded strings with @res
+        if(n.isOneTime()){
+            isOneTimeView.setText("Jednorazowa: tak");
+        }else{
+            isOneTimeView.setText("Jednorazowa: nie");
+        }
+
+        if(n.isVolumeChangeOn()){
+            volumeChangeView.setText("Ustaw poziom dźwięku na: "+n.getSoundVolume());
+        }else{
+            volumeChangeView.setText("Nie zmieniaj poziomu dźwięku.");
+        }
+
+        if(n.isWifiChangeOn()){
+            wifiChangeView.setText("Włącz wifi: tak");
+        }else{
+            wifiChangeView.setText("Włącz wifi: nie");
+        }
+
+        if(n.isBluethoothChangeOn()){
+            bluetoothChangeView.setText("Włącz bluetooth: tak");
+        }else{
+            bluetoothChangeView.setText("Włącz bluetooth: tak");
+        }
+
+        if(n.isAlarmSoundOn()){
+            alarmSoundOnView.setText("Powiadomienie dźwiękowe: tak");
+        }else{
+            alarmSoundOnView.setText("Powiadomienie dźwiękowe: nie");
+        }
+
+        for(TextView v : views){
+            v.setTextSize(EXP_TXT_SIZE);
+            v.setPadding(50, 10, 50, 10);
+            linearLayout.addView(v);
+        }
+        buttons.addView(deleteButton);
+        buttons.addView(editButton);
+        linearLayout.addView(buttons);
+
+
+        return linearLayout;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
--- app/src/main/java/com/harp0n/notifyme/Main_Activity.java
+++ app/src/main/java/com/harp0n/notifyme/Main_Activity.java
@@ -1,23 +1,58 @@
 package com.harp0n.notifyme;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListView;
 
-////////////////////////////// To dla ciebie Seba, ale potrzebowałem głównej aktywności żeby sprawdzić moją /////////////////
 public class Main_Activity extends AppCompatActivity {
 
-    private Button btnCreate;
+    //TODO exchange for load()
+    private Notify[] tests = new Notify[20];
+    private FloatingActionButton fbEditor;
+    private ExpandableListView expList;
+    private int lastPosition=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_);
+        for(int i=0; i<tests.length; i++){
+            Notify n = new Notify();
+            n.setAlarmSoundOn(true);
+            n.setBluethoothChangeOn(false);
+            n.setDiscription("TEST TEST TEST" + i);
+            n.setName("NOTIFY" + i);
+            n.setNotificationMessage("MESSAGE");
+            n.setOneTime(false);
+            n.setRadius(29.3424);
+            n.setVolumeChangeOn(true);
+            n.setSoundVolume(69);
+            n.setWifiChangeOn(false);
+            n.setX_coordinate(69.696969);
+            n.setY_coordinate(96.969696);
+            tests[i] = n;
+        }
 
-        btnCreate = findViewById(R.id.btnCreate);
+        expList = findViewById(R.id.expList);
+        ExpandableList el = new ExpandableList(this, tests);
+        expList.setAdapter(el);
+        expList.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
 
-        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                if (lastPosition != -1
+                        && groupPosition != lastPosition) {
+                    expList.collapseGroup(lastPosition);
+                }
+                lastPosition = groupPosition;
+            }
+        });
+
+        fbEditor = findViewById(R.id.fbEditor);
+        fbEditor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(Main_Activity.this, NotifyEditor_Activity.class);
