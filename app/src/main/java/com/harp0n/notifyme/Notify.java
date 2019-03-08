f0b27048a1994de3bc2494b652f3027a52493abf
Reevo <radoslawkarbowiak@gmail.com>
2019-03-08 10:53:33 +0100

    Okej jakis konstruktor klasy Notify, porobione GUI do aktywności nr 3 (Edytora powiadomień).

--- app/src/main/java/com/harp0n/notifyme/Notify.java
+++ app/src/main/java/com/harp0n/notifyme/Notify.java
@@ -13,122 +13,57 @@ public class Notify {
     private double radius;
 
     private String name;
-    private String discription;
+    private String description;
     private String notificationMessage;
 
     private boolean isOneTime;
 
     private boolean volumeChangeOn;
-    private boolean wifiChangeOn;
-    private boolean bluethoothChangeOn;
-    private boolean alarmSoundOn;
-
-    private boolean wifiIsOn;
-    private boolean bluethoothIsOn;
-
     private int soundVolume;
 
+    private boolean wifiChangeOn;
+    private boolean wifiIsOn;
 
-    public Notify()
-    {
-        this.setID(this.hashCode());
+    private boolean bluethoothChangeOn;
+    private boolean bluethoothIsOn;
+
+    private boolean phoneDataChangeOn;
+    private boolean phoneDataIsOn;
+
+    private boolean planeModeChangeOn;
+    private boolean planeModeIsOn;
+
+    private boolean alarmSoundChangeOn;
+    private boolean alarmSoundIsOn;
+
+    public Notify(double x_coordinate, double y_coordinate, double radius, String name, String description, String notificationMessage, boolean isOneTime, boolean volumeChangeOn, int soundVolume, boolean wifiChangeOn, boolean wifiIsOn, boolean bluethoothChangeOn, boolean bluethoothIsOn, boolean phoneDataChangeOn, boolean phoneDataIsOn, boolean planeModeChangeOn, boolean planeModeIsOn, boolean alarmSoundChangeOn, boolean alarmSoundIsOn) {
+        this.x_coordinate = x_coordinate;
+        this.y_coordinate = y_coordinate;
+        this.radius = radius;
+        this.name = name;
+        this.description = description;
+        this.notificationMessage = notificationMessage;
+        this.isOneTime = isOneTime;
+        this.volumeChangeOn = volumeChangeOn;
+        this.soundVolume = soundVolume;
+        this.wifiChangeOn = wifiChangeOn;
+        this.wifiIsOn = wifiIsOn;
+        this.bluethoothChangeOn = bluethoothChangeOn;
+        this.bluethoothIsOn = bluethoothIsOn;
+        this.phoneDataChangeOn = phoneDataChangeOn;
+        this.phoneDataIsOn = phoneDataIsOn;
+        this.planeModeChangeOn = planeModeChangeOn;
+        this.planeModeIsOn = planeModeIsOn;
+        this.alarmSoundChangeOn = alarmSoundChangeOn;
+        this.alarmSoundIsOn = alarmSoundIsOn;
+
+        this.ID = this.hashCode();
     }
 
 
-
-
-
-
-    ///////////////////////////// Settery gettery toString - nic ważnego //////////////////////////
+///////////////////////////// Settery gettery toString - nic ważnego //////////////////////////
     //======================================================================================///////
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDiscription() {
-        return discription;
-    }
-
-    public void setDiscription(String discription) {
-        this.discription = discription;
-    }
-
-    public String getNotificationMessage() {
-        return notificationMessage;
-    }
-
-    public void setNotificationMessage(String notificationMessage) {
-        this.notificationMessage = notificationMessage;
-    }
-
-    public boolean isOneTime() {
-        return isOneTime;
-    }
-
-    public void setOneTime(boolean oneTime) {
-        isOneTime = oneTime;
-    }
-
-    public boolean isVolumeChangeOn() {
-        return volumeChangeOn;
-    }
-
-    public void setVolumeChangeOn(boolean volumeChangeOn) {
-        this.volumeChangeOn = volumeChangeOn;
-    }
-
-    public boolean isWifiChangeOn() {
-        return wifiChangeOn;
-    }
-
-    public void setWifiChangeOn(boolean wifiChangeOn) {
-        this.wifiChangeOn = wifiChangeOn;
-    }
-
-    public boolean isBluethoothChangeOn() {
-        return bluethoothChangeOn;
-    }
-
-    public void setBluethoothChangeOn(boolean bluethoothChangeOn) {
-        this.bluethoothChangeOn = bluethoothChangeOn;
-    }
-
-    public boolean isAlarmSoundOn() {
-        return alarmSoundOn;
-    }
-
-    public void setAlarmSoundOn(boolean alarmSoundOn) {
-        this.alarmSoundOn = alarmSoundOn;
-    }
-
-    public boolean isWifiIsOn() {
-        return wifiIsOn;
-    }
-
-    public void setWifiIsOn(boolean wifiIsOn) {
-        this.wifiIsOn = wifiIsOn;
-    }
-
-    public boolean isBluethoothIsOn() {
-        return bluethoothIsOn;
-    }
-
-    public void setBluethoothIsOn(boolean bluethoothIsOn) {
-        this.bluethoothIsOn = bluethoothIsOn;
-    }
-
-    public int getSoundVolume() {
-        return soundVolume;
-    }
-
-    public void setSoundVolume(int soundVolume) {
-        this.soundVolume = soundVolume;
-    }
 
     public int getID() {
         return ID;
@@ -162,22 +97,157 @@ public class Notify {
         this.radius = radius;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNotificationMessage() {
+        return notificationMessage;
+    }
+
+    public void setNotificationMessage(String notificationMessage) {
+        this.notificationMessage = notificationMessage;
+    }
+
+    public boolean isOneTime() {
+        return isOneTime;
+    }
+
+    public void setOneTime(boolean oneTime) {
+        isOneTime = oneTime;
+    }
+
+    public boolean isVolumeChangeOn() {
+        return volumeChangeOn;
+    }
+
+    public void setVolumeChangeOn(boolean volumeChangeOn) {
+        this.volumeChangeOn = volumeChangeOn;
+    }
+
+    public int getSoundVolume() {
+        return soundVolume;
+    }
+
+    public void setSoundVolume(int soundVolume) {
+        this.soundVolume = soundVolume;
+    }
+
+    public boolean isWifiChangeOn() {
+        return wifiChangeOn;
+    }
+
+    public void setWifiChangeOn(boolean wifiChangeOn) {
+        this.wifiChangeOn = wifiChangeOn;
+    }
+
+    public boolean isWifiIsOn() {
+        return wifiIsOn;
+    }
+
+    public void setWifiIsOn(boolean wifiIsOn) {
+        this.wifiIsOn = wifiIsOn;
+    }
+
+    public boolean isBluethoothChangeOn() {
+        return bluethoothChangeOn;
+    }
+
+    public void setBluethoothChangeOn(boolean bluethoothChangeOn) {
+        this.bluethoothChangeOn = bluethoothChangeOn;
+    }
+
+    public boolean isBluethoothIsOn() {
+        return bluethoothIsOn;
+    }
+
+    public void setBluethoothIsOn(boolean bluethoothIsOn) {
+        this.bluethoothIsOn = bluethoothIsOn;
+    }
+
+    public boolean isPhoneDataChangeOn() {
+        return phoneDataChangeOn;
+    }
+
+    public void setPhoneDataChangeOn(boolean phoneDataChangeOn) {
+        this.phoneDataChangeOn = phoneDataChangeOn;
+    }
+
+    public boolean isPhoneDataIsOn() {
+        return phoneDataIsOn;
+    }
+
+    public void setPhoneDataIsOn(boolean phoneDataIsOn) {
+        this.phoneDataIsOn = phoneDataIsOn;
+    }
+
+    public boolean isPlaneModeChangeOn() {
+        return planeModeChangeOn;
+    }
+
+    public void setPlaneModeChangeOn(boolean planeModeChangeOn) {
+        this.planeModeChangeOn = planeModeChangeOn;
+    }
+
+    public boolean isPlaneModeIsOn() {
+        return planeModeIsOn;
+    }
+
+    public void setPlaneModeIsOn(boolean planeModeIsOn) {
+        this.planeModeIsOn = planeModeIsOn;
+    }
+
+    public boolean isAlarmSoundChangeOn() {
+        return alarmSoundChangeOn;
+    }
+
+    public void setAlarmSoundChangeOn(boolean alarmSoundChangeOn) {
+        this.alarmSoundChangeOn = alarmSoundChangeOn;
+    }
+
+    public boolean isAlarmSoundIsOn() {
+        return alarmSoundIsOn;
+    }
+
+    public void setAlarmSoundIsOn(boolean alarmSoundIsOn) {
+        this.alarmSoundIsOn = alarmSoundIsOn;
+    }
+
     @Override
     public String toString() {
-
         return "Notify{" +
                 "ID=" + ID +
+                ", x_coordinate=" + x_coordinate +
+                ", y_coordinate=" + y_coordinate +
+                ", radius=" + radius +
                 ", name='" + name + '\'' +
-                ", discription='" + discription + '\'' +
+                ", description='" + description + '\'' +
                 ", notificationMessage='" + notificationMessage + '\'' +
                 ", isOneTime=" + isOneTime +
                 ", volumeChangeOn=" + volumeChangeOn +
-                ", wifiChangeOn=" + wifiChangeOn +
-                ", bluethoothChangeOn=" + bluethoothChangeOn +
-                ", alarmSoundOn=" + alarmSoundOn +
-                ", wifiIsOn=" + wifiIsOn +
-                ", bluethoothIsOn=" + bluethoothIsOn +
                 ", soundVolume=" + soundVolume +
+                ", wifiChangeOn=" + wifiChangeOn +
+                ", wifiIsOn=" + wifiIsOn +
+                ", bluethoothChangeOn=" + bluethoothChangeOn +
+                ", bluethoothIsOn=" + bluethoothIsOn +
+                ", phoneDataChangeOn=" + phoneDataChangeOn +
+                ", phoneDataIsOn=" + phoneDataIsOn +
+                ", planeModeChangeOn=" + planeModeChangeOn +
+                ", planeModeIsOn=" + planeModeIsOn +
+                ", alarmSoundChangeOn=" + alarmSoundChangeOn +
+                ", alarmSoundIsOn=" + alarmSoundIsOn +
                 '}';
     }
 }
--- app/src/main/java/com/harp0n/notifyme/NotifyEditor_Activity.java
+++ app/src/main/java/com/harp0n/notifyme/NotifyEditor_Activity.java
@@ -2,12 +2,62 @@ package com.harp0n.notifyme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class NotifyEditor_Activity extends AppCompatActivity {
+
+    Notify notification;
+
+    Button btnBt, btnVolume, btnData, btnPlane, btnWifi;
+
+    Switch sBt, sData, sPlane, sWifi, sOneTimeManyTimes;
+
+    EditText etName, etDescription, etMessage;
+
+    String notifyName;
+    String notifyDescription;
+
+    boolean isBtClicked = false, isVolumeClicked = false, isDataClicked = false, isPlaneClicked = false, isWifiClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notify_editor_);
+        btnBt = findViewById(R.id.btnBluetooth);
+        btnVolume = findViewById(R.id.btnVolume);
+        btnData = findViewById(R.id.btnPhoneData);
+        btnPlane = findViewById(R.id.btnPlaneMode);
+        btnWifi = findViewById(R.id.btnWifi);
+
+        etName = findViewById(R.id.et_Name);
+        etDescription = findViewById(R.id.et_Description);
+        etMessage = findViewById(R.id.et_NotifyMessage);
+
+        sBt = findViewById(R.id.switchBluetooth);
+        sData = findViewById(R.id.switchPhoneData);
+        sPlane = findViewById(R.id.switchPlaneMode);
+        sWifi = findViewById(R.id.switchWifi);
+        sOneTimeManyTimes = findViewById(R.id.switchIsOneTime);
+
+
+
+
+
+
+
+
     }
+
+
+    public Notify createNotify()
+    {
+        Notify createdNotification = new Notify();
+
+        return createdNotification;
+    }
+
+
+
 }
